cce9d8d87477f64deb41c2bad95017086f5d8c31
Rechie Lagria <41753498+foxching@users.noreply.github.com>
2020-02-22 11:21:00 +0800

    Start coundown

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -1,14 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import Clock from "./Clock";
 import CountdownForm from "./CountdownForm";
 import Main from "./Main";
 
 const Countdown = () => {
   const [count, setCount] = useState(0);
+  const [countdownStatus, setCountdownStatus] = useState("stopped");
 
   const onSetCountdown = seconds => {
     setCount(seconds);
+    setCountdownStatus("started");
   };
+
+  useEffect(() => {
+    let interval;
+
+    if (countdownStatus === "started") {
+      interval = setInterval(() => {
+        let newCount = count - 1;
+        setCount(() => (newCount >= 0 ? newCount : 0));
+      }, 1000);
+    }
+
+    return () => clearInterval(interval);
+  }, [count, countdownStatus]);
 
   return (
     <Main>
