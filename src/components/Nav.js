69c566b1d3f988107f475753082b7ab1127431ed
Rechie Lagria <41753498+foxching@users.noreply.github.com>
2020-02-25 23:41:12 +0800

    fix some code

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,31 +3,46 @@ import { NavLink } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <div className="top-bar">
-      <div className="top-bar-left">
-        <ul className="menu">
-          <li className="menu-text">React Timer App</li>
-          <li>
-            <NavLink to="/" activeClassName="active-link">
-              Timer
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/countdown" activeClassName="active-link">
-              Countdown
-            </NavLink>
-          </li>
-        </ul>
+    <div>
+      <div
+        className="title-bar hide-for-medium"
+        data-responsive-toggle="example-menu"
+        data-hide-for="medium"
+      >
+        <button
+          className="menu-icon"
+          type="button"
+          data-toggle="example-menu"
+        />
+        <div className="title-bar-title">Menu</div>
       </div>
-      <div className="top-bar-right">
-        <ul className="menu">
-          <li className="menu-text">
-            Created by{" "}
-            <a href="http://www.mead.io" target="_blank">
-              Rechie Lagria
-            </a>
-          </li>
-        </ul>
+
+      <div className="top-bar" id="example-menu">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Timer App</li>
+            <li>
+              <NavLink to="/" activeClassName="active-link">
+                Timer
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/countdown" activeClassName="active-link">
+                Countdown
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <ul className="menu">
+            <li className="menu-text">
+              Created by{" "}
+              <a href="http://www.mead.io" target="_blank">
+                Rechie Lagria
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
